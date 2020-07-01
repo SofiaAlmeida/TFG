9a3c6793a4daa07451145a3e129358e74fea74df
SofiaAlmeida <sofiil97@gmail.com>
2020-07-01 16:16:54 +0100

    Añade licencia

--- src/profiling.py
+++ src/profiling.py
@@ -1,5 +1,19 @@
 '''
    Pruebas para comparar los tiempos de ejecución de dos implementaciones de estimadores de la entropía y de la información mutua.
+    Copyright (C) 2020  Sofía Almeida
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
 import entropy_estimators as ee
